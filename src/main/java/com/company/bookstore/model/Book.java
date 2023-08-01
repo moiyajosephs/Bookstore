b902e46eb16040db39dabd8bfce3b6ef69a4b393
Moiya Josephs <josephsmoiya@gmail.com>
2023-07-31 20:00:29 -0400

    added controller routes, repo func and book model

--- src/main/java/com/company/bookstore/model/Book.java
+++ src/main/java/com/company/bookstore/model/Book.java
@@ -1,4 +1,34 @@
 package com.company.bookstore.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@Table(name = "Book")
 public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "book_id")
+    private int id;
+
+    private int isbn;
+
+    private Date publish_date;
+
+    //One to Many , one author to many books?
+    private int author_id;
+
+    private String title;
+
+    //One to one (?)
+    private int publisher_id;
+
+    // double or float?
+    private double price;
+
+
 }
--- src/main/java/com/company/bookstore/repository/BookRepository.java
+++ src/main/java/com/company/bookstore/repository/BookRepository.java
@@ -1,4 +1,13 @@
 package com.company.bookstore.repository;
 
-public interface BookRepository {
+import com.company.bookstore.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface BookRepository extends JpaRepository<Book, Integer> {
+
+
+    //Search Book by Author Id
+    List<Book> findBookByAuthorId(int authorId);
 }
