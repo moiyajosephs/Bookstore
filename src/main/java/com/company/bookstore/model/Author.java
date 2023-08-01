78cee07127110bee7f506ec85949f6076102cfea
PreettamM03 <preetham.madesh03@gmail.com>
2023-08-01 18:19:23 -0400

    Author changes

--- src/main/java/com/company/bookstore/model/Author.java
+++ src/main/java/com/company/bookstore/model/Author.java
@@ -1,4 +1,141 @@
 package com.company.bookstore.model;
 
-public class Author {
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@Table(name = "author")
+public class Author implements Serializable {
+    @Id
+    @Column(name = "author_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
+    @JoinColumn(name = "authorId")
+    private Set<Book> Books = new HashSet<>();
+
+    private String firstName;
+    private String lastName;
+    private String street;
+    private String city;
+    private String state;
+    private int postalCode;
+    private String phoneNumber;
+    private String email;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Set<Book> getBooks() {
+        return Books;
+    }
+
+    public void setBooks(Set<Book> books) {
+        Books = books;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public int getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(int postaCode) {
+        this.postalCode = postaCode;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Author)) return false;
+        Author author = (Author) o;
+        return getId() == author.getId() && getPostalCode() == author.getPostalCode() && Objects.equals(getBooks(), author.getBooks()) && Objects.equals(getFirstName(), author.getFirstName()) && Objects.equals(getLastName(), author.getLastName()) && Objects.equals(getStreet(), author.getStreet()) && Objects.equals(getCity(), author.getCity()) && Objects.equals(getState(), author.getState()) && Objects.equals(getPhoneNumber(), author.getPhoneNumber()) && Objects.equals(getEmail(), author.getEmail());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getBooks(), getFirstName(), getLastName(), getStreet(), getCity(), getState(), getPostalCode(), getPhoneNumber(), getEmail());
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", Books=" + Books +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", street='" + street + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", postaCode=" + postalCode +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
--- src/main/java/com/company/bookstore/repository/AuthorRepository.java
+++ src/main/java/com/company/bookstore/repository/AuthorRepository.java
@@ -1,4 +1,9 @@
 package com.company.bookstore.repository;
 
-public interface AuthorRepository {
+import com.company.bookstore.model.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuthorRepository extends JpaRepository<Author, Integer> {
 }
