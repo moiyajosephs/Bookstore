91bddda0984469cdf48815105f8520b9e309588e
Moiya Josephs <josephsmoiya@gmail.com>
2023-07-31 20:07:22 -0400

    add getter and setter in book, wip book contr test

--- src/main/java/com/company/bookstore/model/Book.java
+++ src/main/java/com/company/bookstore/model/Book.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import javax.persistence.*;
 import java.util.Date;
+import java.util.Objects;
 
 @Entity
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
@@ -30,5 +31,85 @@ public class Book {
     // double or float?
     private double price;
 
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(int isbn) {
+        this.isbn = isbn;
+    }
+
+    public Date getPublish_date() {
+        return publish_date;
+    }
+
+    public void setPublish_date(Date publish_date) {
+        this.publish_date = publish_date;
+    }
+
+    public int getAuthor_id() {
+        return author_id;
+    }
+
+    public void setAuthor_id(int author_id) {
+        this.author_id = author_id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getPublisher_id() {
+        return publisher_id;
+    }
+
+    public void setPublisher_id(int publisher_id) {
+        this.publisher_id = publisher_id;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return id == book.id && isbn == book.isbn && author_id == book.author_id && publisher_id == book.publisher_id && Double.compare(book.price, price) == 0 && Objects.equals(publish_date, book.publish_date) && Objects.equals(title, book.title);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, isbn, publish_date, author_id, title, publisher_id, price);
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", isbn=" + isbn +
+                ", publish_date=" + publish_date +
+                ", author_id=" + author_id +
+                ", title='" + title + '\'' +
+                ", publisher_id=" + publisher_id +
+                ", price=" + price +
+                '}';
+    }
 }
